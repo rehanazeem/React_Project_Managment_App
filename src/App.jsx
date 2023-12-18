5f4eaf7a93d8f780b0150a7f1a1a1f26da48baf1
Rehan Azeem <92761969+rehanazeem@users.noreply.github.com>
2023-12-18 10:48:01 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectID: undefined,
+    projects: []
+  });
+  return (
+    <main className="h-screen my-8 flex gap-8 ">
+      <ProjectsSidebar />
+      <NoProjectSelected />
+    </main>
+  );
+}
+
+export default App;
