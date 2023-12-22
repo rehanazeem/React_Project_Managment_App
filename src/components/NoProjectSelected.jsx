07c16cc7f8ffc2fe92060b51878634f83eb5b0a2
Rehan <rehanzzz1590@gmail.com>
2023-12-22 18:06:46 +0500

    fixes

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -3,16 +3,16 @@ import Button from "./Button.jsx";
 
 export default function NoProjectSelected() {
   return (
-    <div className="mt-24 text-center w-2/3">
+    <div className="w-2/3 mt-24 text-center">
       <img
         src={noProjectImage}
         alt="An empty task list"
-        className="w-16 h-16 object-contain mx-auto"
+        className="object-contain w-16 h-16 mx-auto"
       />
-      <h2 className="text-xl font-bold text-stone-500 my-4">
+      <h2 className="my-4 text-xl font-bold text-stone-500">
         No project Selected
       </h2>
-      <p className="text-stone-400 mb-4">
+      <p className="mb-4 text-stone-400">
         Select a project or get started with a new one
       </p>
       <p className="mt-8">
